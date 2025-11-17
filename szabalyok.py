326d1ba1badfec1e7075b9f7db69e429c7bd1d0f
Deák Máté <b3deamat@vasvari.org>
2025-11-17 14:18:48 +0100

    Code2

--- szabalyok.py
+++ szabalyok.py
@@ -1,6 +1,20 @@
+import unittest
+
 a=int(input("adjon megy egy számot"))
 b=int(input("adjon meg még egy számot"))
 szamolas=a+b
 szorzas=a*b
 print(szorzas)
 print(szamolas)
+
+class Testek(unittest.TestCase):
+    def test_nagyobb_nulla(self):
+        nagyobb=int(szamolas)
+        self.assertGreater(nagyobb,0)
+
+class Testek2(unittest.TestCase):
+    def test_nagyobb_nulla(self):
+        nagyobb=int(szorzas)
+        self.assertGreater(nagyobb,5)
+
+unittest.main()
